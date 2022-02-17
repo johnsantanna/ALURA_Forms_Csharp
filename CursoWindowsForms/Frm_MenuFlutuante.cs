3c68a67686172b54a59cad16d96ec7c00a90ccfa
John Santanna <john.santanna@outlook.com>
2022-02-16 23:35:33 -0300

    Adicionar arquivos de projeto.

---
+++ CursoWindowsForms/Frm_MenuFlutuante.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_MenuFlutuante : Form
+    {
+        public Frm_MenuFlutuante()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_MenuFlutuante_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                var contextMenu = new ContextMenuStrip();
+                var vToolTip1 = new ToolStripMenuItem() { Text = "Item do menu 1", Image = Image.FromFile("C:\\WindowsForms\\Curso\\CursoWindowsForms\\CursoWindowsForms\\Imagens\\key.png") };
+                var vToolTip2 = new ToolStripMenuItem() { Text = "Item do menu 2", Image = Image.FromFile("C:\\WindowsForms\\Curso\\CursoWindowsForms\\CursoWindowsForms\\Imagens\\Frm_ValidaSenha.png") };
+                contextMenu.Items.Add(vToolTip1);
+                contextMenu.Items.Add(vToolTip2);
+                contextMenu.Show(this, new Point(e.X, e.Y));
+                vToolTip1.Click += new EventHandler(vToolTip1_Click);
+                vToolTip2.Click += new EventHandler(vToolTip2_Click);
+            }
+
+            void vToolTip1_Click(object sender1, EventArgs e1)
+            {
+                MessageBox.Show("Pressionei a opção do menu 1");
+            }
+
+            void vToolTip2_Click(object sender2, EventArgs e2)
+            {
+                MessageBox.Show("Pressionei a opção do menu 2");
+            }
+
+            ToolStripMenuItem DesenhaItemMenu(string text, string nomeImagem)
+            {
+                var vToolTip = new ToolStripMenuItem();
+                vToolTip.Text = text;
+
+                Image image = (Image)Properties.Resources.ResourceManager.GetObject(nomeImagem);
+
+                vToolTip.Image = image;
+                return vToolTip;
+            }
+
+        }
+    }
+}
---
+++ CursoWindowsFormsBiblioteca/DataBases/FicharioDB.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class FicharioDB
+    {
+        public string mensagem;
+        public bool status;
+        public string tabela;
+        public LocalDBClass db;
+
+        public FicharioDB(string Tabela)
+        {
+            status = true;
+            try
+            {
+                db = new LocalDBClass();
+                tabela = Tabela;
+                mensagem = "Conexão com a Tabela criada com sucesso";
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Conexão com a Tabela com erro: " + ex.Message;
+            }
+        }
+
+        public void Incluir(string Id, string jsonUnit)
+        {
+            status = true;
+            try
+            {
+                // INSERT INTO Cliente (Id, JSON) VALUES ('00001','{...}')
+
+                var SQL = $"INSERT INTO {tabela} (Id, JSON) VALUES ('{Id}','{jsonUnit}')";
+                db.SQLCommand(SQL);
+                status = true;
+                mensagem = "Inclusão efetuada com sucesso. Identificador: " + Id;
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Ocorreu o seguinte erro: " + ex.Message;
+            }
+        }
+        public string Buscar(string Id)
+        {
+            status = true;
+            try
+            {
+                var SQL = $"SELECT * FROM {tabela} WHERE Id = '{Id}')";
+                var dt = db.SQLQuery(SQL);
+                if (dt.Rows.Count > 0)
+                {
+                    string conteudo = dt.Rows[0]["JSON"].ToString();
+                    status = true;
+                    mensagem = "Busca efetuada com sucesso. Identificador: " + Id;
+                    return conteudo;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Identificador não existe: " + Id;
+                }
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+            }
+            return "";
+        }
+        public List<string> BuscarTodos()
+        {
+            status = true;
+            List<string> List = new List<string>();
+            try
+            {
+                var SQL = $"SELECT * FROM {tabela})";
+                var dt = db.SQLQuery(SQL);
+
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count - 1; i++)
+                    {
+                        string conteudo = dt.Rows[i]["JSON"].ToString();
+                        List.Add(conteudo);
+                    }
+                    return List;
+                }
+                else
+                {
+                    status = false;
+                    mensagem = "Não existem clientes na base de dados";
+                }
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Erro ao buscar os dados: " + ex.Message;
+            }
+            return List;
+        }
+
+    }
+}
